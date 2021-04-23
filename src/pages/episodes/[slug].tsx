3411275e0a68c3c027daa0bf45504e96c021f346
Luisa Dirce <luisa.dirce.silva@gmail.com>
2021-04-23 18:59:40 -0300

    Page titles and favicon added

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -9,6 +9,7 @@ import Link from 'next/link';
 import { usePlayer } from '../../contexts/PlayerContext'
 
 import styles from './episode.module.scss';
+import Head from 'next/head';
 
 type Episode = {
   id: string;
@@ -30,6 +31,9 @@ export default function Episode ({ episode }: EpisodeProps) {
   const { play } = usePlayer();
   return (
     <div className={styles.episode}>
+      <Head>
+       <title>{episode.title} | Podcastr</title>
+      </Head>
       <div className={styles.thumbnailContainer}>
         <Link href="/">
           <button type="button">
